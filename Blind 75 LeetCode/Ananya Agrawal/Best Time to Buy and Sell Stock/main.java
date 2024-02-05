fd9c4edb63bc3663d33c7da2fb67689e2dae03ab
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2024-02-05 22:07:56 +0530

    Update main.java

--- Blind 75 LeetCode/Ananya Agrawal/Best Time to Buy and Sell Stock/main.java
+++ Blind 75 LeetCode/Ananya Agrawal/Best Time to Buy and Sell Stock/main.java
@@ -1,19 +1,14 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int profit=0;// at the beginning the profit is zero
-        int buyPrice=prices[0];// at the beginning the minimum price is first price
-        for(int i=1;i<prices.length;i++){
-            // if current price is less than buy price we will update
-            if(prices[i]<buyPrice){
-                buyPrice=prices[i];
-            }
-            else{
-                int currPrice=prices[i]-buyPrice;
-                profit=Math.max(currPrice,profit);
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else {
+                maxProfit = Math.max(maxProfit, prices[i] - minPrice);
             }
         }
-        return profit;
-
-        
+        return maxProfit;
     }
 }
