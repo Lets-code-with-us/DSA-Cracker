0f761f4b3cbffd7ff09c9208e8ae4ecf2306dd11
sarniu <108914680+sarniu@users.noreply.github.com>
2024-02-07 21:10:34 +0530

    Update code.py

--- Blind 75 LeetCode/Pankajavalli RV/Product of Array Except Self/code.py
+++ Blind 75 LeetCode/Pankajavalli RV/Product of Array Except Self/code.py
@@ -1,1 +1,19 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        
+        prefix = [1]
+        postfix = [1]
 
+        for i in range(0,len(nums) ):
+            val = prefix[-1] * nums[i]
+            prefix.append(val)
+        
+        for i in range(len(nums) - 1, -1, -1):                        
+            val = nums[i] * postfix[0]
+            postfix = [val] + postfix
+          
+        res = []  
+      
+        for i in range(0,len(nums)):
+            res.append(prefix[i] * postfix[i+1])
+        return res
