caa0534aed36fead07d32e5c0042dd6de48633bd
Himanshu Khurana <126713985+himanshukhurana2324@users.noreply.github.com>
2024-03-03 23:38:27 +0530

    new file:   Blind 75 LeetCode/Himanshu Khurana/search_in_the_rotated_array/Screenshot 2024-03-03 233547.png 	new file:   Blind 75 LeetCode/Himanshu Khurana/search_in_the_rotated_array/Solution.java

---
+++ Blind 75 LeetCode/Himanshu Khurana/search_in_the_rotated_array/Solution.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int search(int[] nums, int target) {
+      int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            // Check if the left half is sorted
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            // Otherwise, the right half is sorted
+            else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
